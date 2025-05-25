96b97c5027a4662b47ad271e0d825193d4ac2384
muhamsh <muhammad-ashraful@outlook.com>
2025-05-25 19:26:30 +0600

    feat(written): moving to write the answers

--- solution.sql
+++ solution.sql
@@ -10,8 +10,8 @@ CREATE TABLE rangers (
 -- Create species table
 CREATE TABLE species (
   species_id SERIAL PRIMARY KEY,
-  common_name VARCHAR(100) NOT NULL,
-  scientific_name VARCHAR(150) NOT NULL,
+  common_name VARCHAR(100) UNIQUE NOT NULL,
+  scientific_name VARCHAR(150) UNIQUE NOT NULL,
   discovery_date DATE NOT NULL,
   conservation_status VARCHAR(50) CHECK (conservation_status IN ('Endangered', 'Vulnerable')) NOT NULL
 );
@@ -35,6 +35,25 @@ SELECT * FROM species;
 DROP TABLE species;
 SELECT * FROM sightings;
 DROP TABLE sightings;
+
+-- Insert sample data 
+INSERT INTO rangers (name, region) VALUES 
+('Alice Green', 'Northern Hills '),
+('Bob Smith', ' River Delta '),
+('Carol King ',' Mountain Range ');
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
+INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(4, 1, 2, 'Snowfall Pass', '2024-05-18 18:30:00');
+INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_time, notes) VALUES
+ (111, 210, 32, 'Bamboo Grove East', '2024-05-15 09:10:00', NULL);
+
 
 --solution
 
